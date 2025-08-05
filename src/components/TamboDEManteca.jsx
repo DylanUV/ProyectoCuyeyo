7a8a2a5edf41625b04a80ec630a60b72935c7ea5
ChaboCode <kaerdos.dev@protonmail.com>
2025-08-05 17:50:57 -0600

    Thumb load instead of full file

--- src/components/TamboDEManteca.jsx
+++ src/components/TamboDEManteca.jsx
@@ -1,16 +1,30 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 export default function TamboDeManteca() {
-  const [imgs, setImgs] = useState()
+  const [imgs, setImgs] = useState();
 
   useEffect(() => {
-    fetch('https://rotoplas.kaerdos.dev/api/files/list?page=1&count=5')
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        setImgs(data.files.map(el => <img style={{ 'width': '300px', 'height': '300px', 'object-fit': 'contain' }} src={`https://rotoplas.kaerdos.dev/files/${el.Name}`} alt="sexo en el oxxo" />))
-      })
-  }, [])
+    fetch("https://rotoplas.kaerdos.dev/api/files/list?page=1&count=5")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setImgs(
+          data.files.map((el) => {
+            return (
+              <a target="_blank" href={`https://rotoplas.kaerdos.dev/files/${el.Name}`} rel="noreferrer">
+                <img
+                  style={{
+                    width: "300px",
+                    height: "100px",
+                    "object-fit": "contain",
+                  }}
+                  src={`https://rotoplas.kaerdos.dev/thumbs/${el.Name}`}
+                  alt="sexo en el oxxo" />
+              </a>
+            );
+          })
+        );
+      });
+  }, []);
 
-
-  return imgs
+  return imgs;
 }
