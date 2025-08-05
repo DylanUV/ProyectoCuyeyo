21c9eec83033c8fc0c64c8790c46e3ea2b32454c
ChaboCode <kaerdos.dev@protonmail.com>
2025-08-05 17:51:08 -0600

    Table and step input

--- src/components/WhenEresElCristian.jsx
+++ src/components/WhenEresElCristian.jsx
@@ -1,36 +1,70 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 export default function WhenEresElCristian() {
-  const [result, setResult] = useState(0)
-  const [imagen, setImagen] = useState()
+  const [result, setResult] = useState(0);
+  const [data, setData] = useState(null);
+  const [imagen, setImagen] = useState();
 
   function check(formData) {
     const params = new URLSearchParams({
-      f_str: formData.get('f'),
-      x0: formData.get('x0'),
-      y0: formData.get('y0'),
-      h: formData.get('h'),
-      end: formData.get('end'),
-    })
+      f_str: formData.get("f"),
+      x0: formData.get("x0"),
+      y0: formData.get("y0"),
+      steps: formData.get("steps"),
+      end: formData.get("end"),
+    });
 
+    console.log(params.toString());
 
-    console.log(params.toString())
+    fetch(`https://runge-kutta.kaerdos.dev/values?` + params)
+      .then((res) => res.json())
+      .then((data) => {
+        setResult(data.result);
+        setData(data);
+      });
 
-    fetch(`https://runge-kutta.kaerdos.dev/grunt-kutta?` + params
-    ).then(res => res.json()).then(data => setResult(data.result))
-
-    setImagen(`https://runge-kutta.kaerdos.dev/img?` + params)
+    setImagen(`https://runge-kutta.kaerdos.dev/img?` + params);
   }
 
   return (
     <form action={check}>
-      <input required name="f" type='text' placeholder="Funcion" />
-      <input required name="x0" type='text' placeholder="x0" />
-      <input required name="y0" type='text' placeholder="y0" />
-      <input required name="h" type='text' placeholder="h (paso)" />
-      <input required name="end" type='texxt' placeholder="X maxima" />
-      <input type='submit' />
+      <input required name="f" type="text" placeholder="Funcion" />
+      <input required name="x0" type="text" placeholder="x0" />
+      <input required name="y0" type="text" placeholder="y0" />
+      <input required name="steps" type="number" placeholder="Pasos" />
+      <input required name="end" type="text" placeholder="X maxima" />
+      <input type="submit" />
       <span>Resultado: {result}</span>
-      <img src={imagen} alt=''></img>
+      <img src={imagen} alt=""></img>
+
+      <table id="runge-kutta-vals" border="1">
+        <thead>
+          <tr>
+            <th>X</th>
+            <th>Y</th>
+            <th>k1</th>
+            <th>k2</th>
+            <th>k3</th>
+            <th>k4</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data &&
+            data.k1.map((y, i) => (
+              <tr>
+                <td>{data.xn[i].toFixed(4)}</td>
+                <td>{data.steps[i].toFixed(4)}</td>
+                <td>{data.k1[i].toFixed(4)}</td>
+                <td>{data.k2[i].toFixed(4)}</td>
+                <td>{data.k3[i].toFixed(4)}</td>
+                <td>{data.k4[i].toFixed(4)}</td>
+              </tr>
+            ))}
+            <tr>
+              <td>{data.xn[data.xn.length - 1].toFixed(4)}</td>
+              <td>{data.steps[data.xn.length - 1].toFixed(4)}</td>
+            </tr>
+        </tbody>
+      </table>
     </form>
-  )
+  );
 }
